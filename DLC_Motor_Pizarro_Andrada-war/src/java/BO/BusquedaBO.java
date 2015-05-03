6dec4c1e15071ed657339fad69f512c12613534c
JoaquinPizarro <joaquin.alejandro.pizarro@gmail.com>
2015-05-03 18:14:24 -0300

    primer commit
    primer commit con el modelo inicial del tp

---
+++ DLC_Motor_Pizarro_Andrada-war/src/java/BO/BusquedaBO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BO;
+
+/**
+ *
+ * @author Juaco
+ */
+public interface BusquedaBO {
+    
+    //funcion que vamos a implementar.
+    void validaBusqueda(BeanBusqueda obj);
+    
+    
+}
